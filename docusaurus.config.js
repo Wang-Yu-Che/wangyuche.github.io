60e84726d177a92a6a58e6e06002186378c90ad3
王宇瞮 <2863424897@qq.com>
2024-08-29 21:11:42 +0800

    yes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'wangyuche', // Usually your GitHub org/user name.
+  organizationName: 'Wang-Yu-Che', // Usually your GitHub org/user name.
   projectName: 'my-blog', // Usually your repo name.
   deploymentBranch: "gh-pages",
 
